6d2669264b825f65c8981af36c3c57ea82c89b5f
unknown <irakielvillamizar@gmail.com>
2026-04-25 20:51:45 -0400

    arregle el msj

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Sustituye 'tu-usuario' por tu nombre de GitHub
   // Sustituye 'nombre-del-repo' por el nombre de tu repositorio
-  site: 'https://tu-usuario.github.io',
-  base: '/nombre-del-repo',
+  site: 'https://irakielvilla.github.io',
+  base: '/guia1',
 });
